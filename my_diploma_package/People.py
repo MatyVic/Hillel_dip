8b176ef4eaadf69ba4e36a3dc359b268abd8537a
MatyVic <thexcvb@gmail.com>
2026-03-15 14:07:31 +0200

    third commit

---
+++ my_diploma_package/People.py
@@ -0,0 +1,10 @@
+import datetime
+
+class Human:
+    def __init__(self, name, surname, date_of_birth, death_of_death, sex):
+        self.name = name
+        self.surname = surname
+        self.date_of_birth = date_of_birth
+        if not death_of_death == 0:
+            self.death_of_death = death_of_death
+
